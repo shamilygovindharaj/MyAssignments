383cd340359ffee94b7011b4413f0ff90a28d7a1
shamilygovindharaj <shamilygovindharaj@gmail.com>
2026-06-21 16:07:47 +0530

    Add files via upload
    Day1

---
+++ Day1.js
@@ -0,0 +1,38 @@
+/* let firstName="shamily"
+let companyName="NT"
+let mobilenNumber= 87623682
+let isAutomation=true
+let hasPlaywright
+
+console.log("firstName is: " +firstName , "type of firstname is : " +typeof firstName,)
+console.log("companyname is: " +companyName, "type of companyname is : "+typeof companyName,companyName)
+console.log("mob number is: " +mobilenNumber, "type of mobnumber is: " +typeof mobilenNumber,mobilenNumber)
+console.log(typeof isAutomation,isAutomation)
+console.log(typeof hasPlaywright)
+
+ */
+
+function add(){
+
+    const a=10
+    const b=20
+    const c=a+b
+    console.log(c)
+   
+}
+add()
+
+
+var boo = true
+
+if(boo==true){
+    console.log(boo)
+}
+
+
+if(true){
+    var num = 10
+}
+console.log(num)
+ 
+
